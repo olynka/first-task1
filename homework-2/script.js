f2b15ceb6aa8f433fde1732e56bd3c739703d7c2
olynka <98587532+olynka@users.noreply.github.com>
2023-03-09 22:35:23 +0200

    fix Homework-2

--- homework-2/script.js
+++ homework-2/script.js
@@ -1,43 +1,90 @@
+// version 1
 
-
+// перше число від якого відштовхуємося
 const N = +prompt('Введіть число N');
 const auditN = Number.isInteger(N);
-console.log(`перевірка на ціле число N ${auditN}`);
 
-
-
+// друге число до якого проганяємо цикл
 const M = +prompt('Введіть число M');
 const auditM = Number.isInteger(M);
-console.log(`перевірка на ціле число N ${auditM}`);
+
+let sum = 0;
+
+// перевіряємо чи цілі  числа циклом while
+while ((!auditN)||!N>0) {
+  alert(`Введіть цілі числа`)
+}
+
+while ((!auditM)||!M>N) {
+  alert(`Введіть цілі числа`)
+}
+
+// отримуємо інформацію отримуємо інформацію про те чи потрібно пропускати парні числа
+const IsSteamy = confirm("Потрібно пропускати парні?");
 
 
-if ((!auditN)||!(auditM)) {
-     alert(`Введіть цілі числа`)
-   
-} else {
-
-    const h = confirm("Потрібно пропускати парні?");
-
-     function calculateTotal(a, b, h) {
-    let sum = 0;
-    
-    if (h) {
+// IsSteamy дає нам булеве значення,і в залежності від того вираховуємо суму
+    if (IsSteamy) {
       
-    for (let i = a; i <= b; i += 1) {
+    for (let i = N; i <= M; i += 1) {
         if (!(i % 2 == 0)) {
             sum += i;
+           
         }
     }
 } else {
-    for (let i = a; i <= b; i += 1) {
-            sum += i;
+    for (let i = N; i <= M; i += 1) {
+        sum += i;
+       
     }
 }
-return sum
-}
-  document.writeln(`Cума чисел ${calculateTotal(N,M,h)}`);
+document.writeln(`N=${N},  M=${M} , виключаємо парні числа?= ${IsSteamy} ,  сума чисел  ${sum}`)
+ 
+
+
+
+
+
+// version 2
+// const N = +prompt('Введіть число N');
+// const auditN = Number.isInteger(N);
+// console.log(`перевірка на ціле число N ${auditN}`);
+
+
+
+// const M = +prompt('Введіть число M');
+// const auditM = Number.isInteger(M);
+// console.log(`перевірка на ціле число N ${auditM}`);
+
+
+// if ((!auditN)||!(auditM)) {
+//      alert(`Введіть цілі числа`)
    
-}
+// } else {
+
+//     const h = confirm("Потрібно пропускати парні?");
+
+//      function calculateTotal(a, b, h) {
+//     let sum = 0;
+    
+//     if (h) {
+      
+//     for (let i = a; i <= b; i += 1) {
+//         if (!(i % 2 == 0)) {
+//             sum += i;
+//         }
+//     }
+// } else {
+//     for (let i = a; i <= b; i += 1) {
+//             sum += i;
+//     }
+// }
+// return sum
+// }
+//   document.writeln(`Cума чисел ${calculateTotal(N,M,h)}`);
+   
+// }
+
 
 
 
