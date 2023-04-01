15ec5860d8da2ba57d8cf8151db729dcfd074627
olynka <98587532+olynka@users.noreply.github.com>
2023-04-01 21:20:25 +0300

    improvement

--- homework-9/script.js
+++ homework-9/script.js
@@ -4,41 +4,55 @@ function generateBlocks() {
     const blocks = 25;
 
     for (let i = 0; i < blocks; i++) {
-        const d =document.createElement("div");
-        d.classList.add('box');
-         block.append(d);
-        console.log(d);
+        const div =document.createElement("div");
+        div.classList.add('box');
+         block.append(div);
         
     }
-    let g = document.querySelectorAll('.box');
-    console.log();
-   
-  for (let i= 0; i < g.length; i++) {
-      g[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-      g[i].style.height = "50px";
-      g[i].style.flexBasis = "50px";
-      g[i].style.margin = "10px";
-      g[i].style.borderRadius = "10px";
-      g[i].style.border = "solid black 1px";
+   const bocksChildren = document.querySelectorAll('.box'); 
+    
+  for (let i= 0; i < bocksChildren.length; i++) {
+     bocksChildren[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+     bocksChildren[i].style.height = "50px";
+     bocksChildren[i].style.flexBasis = "50px";
+     bocksChildren[i].style.margin = "10px";
+      bocksChildren[i].style.borderRadius = "10px";
+     bocksChildren[i].style.border = "solid black 1px";
   
   }
  
-  return 3
+  return "квадрати не змінюють колір"
 };
 
 
 function generateBlocksInterval() {
 
     setInterval(() => {
-        let g = document.querySelectorAll('.box');
-        for (let i = 0; i < g.length; i++) {
-            g[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        let bocksChildren = document.querySelectorAll('.box');
+        for (let i = 0; i < bocksChildren.length; i++) {
+           bocksChildren[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
         }
     }, 1000);
     
 
-    return 6
+    return "квадрати змінюють колір"
 }
 
+const button = document.querySelector('.button');
+
+
+
+const handleClick = () => {
+     let g = document.querySelectorAll('.box');
+    generateBlocksInterval()
+  
+    
+};
+
+
+button.addEventListener("click", handleClick);
+
+
+
 console.log(generateBlocks());
-console.log(generateBlocksInterval());
+// console.log(generateBlocksInterval());
